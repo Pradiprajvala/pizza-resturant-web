abdf762140fb5b4c3a8db4c2410dcef23a847d7d
Pradiprajvala <u20ce053@ced.svnit.ac.in>
2022-01-16 21:44:39 +0530

    added products

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React from 'react'
 import {BrowserRouter as Router} from 'react-router-dom'
 import { GlobalStyle } from './globalStyles';
 import Hero from './Components/Hero/index.js'
+import Products  from './Components/Products/index.js'
+import{ productData } from './Components/Products/data.js'
+import Feature from './Components/Feature/index.js';
 function App() {
   return (
     <Router>
       <GlobalStyle />
-      
       <Hero />
+      <Products heading='Choose your favourite' data={productData}/>
+      <Feature />
     </Router>
   );
 }
