6ac2cbb8751da87cd69fd3e0df7b992d241a771f
Pradiprajvala <u20ce053@ced.svnit.ac.in>
2022-01-16 22:11:27 +0530

    sweets added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import { GlobalStyle } from './globalStyles';
 import Hero from './Components/Hero/index.js'
 import Products  from './Components/Products/index.js'
-import{ productData } from './Components/Products/data.js'
+import{ productData,sweetProductData } from './Components/Products/data.js'
 import Feature from './Components/Feature/index.js';
 function App() {
   return (
@@ -13,6 +13,8 @@ function App() {
       <Hero />
       <Products heading='Choose your favourite' data={productData}/>
       <Feature />
+      <Products heading='Sweet' data={sweetProductData}/>
+     
     </Router>
   );
 }
