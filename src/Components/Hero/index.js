0a544d04c4063ecc539e668edf00ac447d7773ef
Pradiprajvala <u20ce053@ced.svnit.ac.in>
2022-01-17 19:07:45 +0530

    login button added-ui

--- src/Components/Hero/index.js
+++ src/Components/Hero/index.js
@@ -1,7 +1,12 @@
 import React, {useState} from 'react'
 import Navbar from '../Navbar'
 import Sidebar from '../Sidebar'
-import {HeroContainer, HeroContent, HeroItems, HeroH1, HeroP, HeroBtn} from './HeroElements'
+import LoginButton from '../LoginButton/index.js'
+import LoginPage from '../../Pages/LoginPage/index.js'
+import {
+  HeroContainer, HeroContent,HeroItems,HeroH1,HeroP,HeroBtn
+} from './HeroElements'
+
 
 const Hero = () => {
   const [isOpen, setIsOpen] = useState(false)
@@ -10,6 +15,7 @@ const Hero = () => {
     setIsOpen(!isOpen)
   }
   return (
+   
         <HeroContainer>
           <Navbar toggle={toggle} />
           <Sidebar isOpen={isOpen} toggle={toggle}/>
@@ -18,9 +24,11 @@ const Hero = () => {
               <HeroH1>Greatest Pizza Ever</HeroH1>
               <HeroP>Ready in 60 seconds</HeroP>
               <HeroBtn>Place Order</HeroBtn>
+            <LoginButton /> 
             </HeroItems>
           </HeroContent>
         </HeroContainer>
+    
     )
 }
 
